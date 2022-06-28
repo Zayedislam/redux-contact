3b9af37ba30d685f22e868a6fbd34a69c8840c2d
Zayedislam <islamzayed75@gmail.com>
2022-06-28 12:04:49 +0600

    upload redux project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+
+import "react-toastify/dist/ReactToastify.css";
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { contactReducer } from "./redux/reducers/contactReducer";
+
+const store = createStore(contactReducer, composeWithDevTools());
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    
     <App />
-  </React.StrictMode>
+  
+      
+    
+  </Provider>,
+  rootElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
